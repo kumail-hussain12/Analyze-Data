c2ff5aed7a8305a6da26c52c8d4690ff75d3f855
Kumail-Hussain-coder <kh478341@gmail.com>
2025-02-15 10:59:22 -0800

    third

--- Task_1/sales_data.py
+++ Task_1/sales_data.py
@@ -17,16 +17,17 @@ print(df.head())
 df.info()
 #print(df.isnull().sum())
 
-#---------------------------------------------------------------------------------------------------
-# First
-# Analyze Sales Trends
-# Total Sales over time
-
 # Remove spaces from column names
 df.columns = df.columns.str.strip()
 print(df.columns)
 
-# Convert date column 
+#---------------------------------------------------------------------------------------------------
+# First
+# Analyze Sales Trends
+# Total Sales over time
+# Monthly Sales Trends 
+
+# Convert orderdate to datetime 
 df['ORDERDATE'] = pd.to_datetime(df["ORDERDATE"])
 
 # Group the sales by month
@@ -34,10 +35,13 @@ monthly_sales = df.groupby(df['ORDERDATE'].dt.to_period('M'))['SALES'].sum()
 print(monthly_sales)
 
 # Plot Sales Trends 
-# Create the graph of Monthly sales Trends and total Sales
-monthly_sales.plot(kind= 'line', title= 'Monthly Sales Trend', figsize = (20,10))
+# Create a Plot bar chart
+monthly_sales.plot(kind= 'line', marker= 'o', color= 'b')
+plt.figure(figsize =(25,12))
+plt.title("Monthly Sales Trend")
 plt.xlabel("Month")
 plt.ylabel("Total Sales")
+plt.grid(True)
 plt.show()
 
 #------------------------------------------------------------------------------------------------
@@ -47,17 +51,35 @@ plt.show()
 top_product = df.groupby('PRODUCTLINE')['SALES'].sum().sort_values(ascending= False)
 print(top_product)
 
-# Create a bar chart
-top_product.plot(kind= 'line', title= 'Top Selling Product', figsize= (20,10))
+# Create a Plot bar chart
+plt.figure(figsize= (25,12))
+top_product.plot(kind= 'bar', color= 'g')
+plt.title("Top Selling Product")
 plt.xlabel("Product Line")
 plt.ylabel("Total Sales")
+plt.xticks(rotation= 45)
 plt.show()
+
+#--------------------------------------------------------------------------------------------
+# Sales By Region
+# Sales by country  
+top_countries = df.groupby('COUNTRY')['SALES'].sum().sort_values(ascending=False)
+
+# Create a Plot bar chart  
+plt.figure(figsize=(25,12))  
+top_countries.plot(kind='bar', color='orange')  
+plt.title("Sales by Country")  
+plt.xlabel("Country")  
+plt.ylabel("Total Sales")  
+plt.xticks(rotation=45)  
+plt.show()
+
 
 #---------------------------------------------------------------------------------------------
 
-# Third
 # Factor Affecting Sales
-sns.scatterplot(x=df['PRICEEACH'], y=df['SALES'])
+plt.figure(figsize=(12,8) )
+sns.scatterplot(x=df['PRICEEACH'], y=df['SALES'], alpha= 0.5)
 
 # Show the Graph
 plt.title("Price vs Sales")
